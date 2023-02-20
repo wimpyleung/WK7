bb79e3e21e10dcc711fdb69e9b8f912f0cda1917
wimpyleung <wingpin.leung@dcmail.ca>
2023-02-20 14:34:50 -0500

    WK7

---
+++ scripts/script.js
@@ -0,0 +1,120 @@
+
+document.getElementById("rr0c0").onclick = function () {
+   document.getElementById("me").src="./images/100897257.jpg";
+}
+
+document.getElementById("rr0c1").onclick = function () {
+   document.getElementById("me").src="./images/100899065.jpg";
+}
+
+document.getElementById("rr0c2").onclick = function () {
+   document.getElementById("me").src="./images/100900107.jpg";
+}
+
+document.getElementById("rr0c3").onclick = function () {
+   document.getElementById("me").src="./images/100899476.jpg";
+}
+
+document.getElementById("rr0c4").onclick = function () {
+   document.getElementById("me").src="./images/100907740.jpg";
+}
+
+document.getElementById("rr0c5").onclick = function () {
+   document.getElementById("me").src="./images/100790823.jpg";
+}
+
+document.getElementById("rr0c6").onclick = function () {
+   document.getElementById("me").src="./images/100604120.jpg";
+}
+
+document.getElementById("rr0c7").onclick = function () {
+   document.getElementById("me").src="./images/100902301.jpg";
+}
+
+document.getElementById("rr0c8").onclick = function () {
+   document.getElementById("me").src="./images/100892802.jpg";
+}
+
+document.getElementById("rr0c9").onclick = function () {
+   document.getElementById("me").src="./images/100886918.jpg";
+}
+
+document.getElementById("rr1c0").onclick = function () {
+   document.getElementById("me").src="./images/100908704.jpg";
+}
+
+document.getElementById("rr1c1").onclick = function () {
+   document.getElementById("me").src="./images/100864595.jpg";
+}
+
+document.getElementById("rr1c2").onclick = function () {
+   document.getElementById("me").src="./images/100875859.jpg";
+}
+
+document.getElementById("rr1c3").onclick = function () {
+   document.getElementById("me").src="./images/100891274.jpg";
+}
+
+document.getElementById("rr1c4").onclick = function () {
+   document.getElementById("me").src="./images/100837128.jpg";
+}
+
+document.getElementById("rr1c5").onclick = function () {
+   document.getElementById("me").src="./images/100907733.jpg";
+}
+
+document.getElementById("rr1c6").onclick = function () {
+   document.getElementById("me").src="./images/100897855.jpg";
+}
+
+document.getElementById("rr1c7").onclick = function () {
+   document.getElementById("me").src="./images/100895530.jpg";
+}
+
+document.getElementById("rr1c8").onclick = function () {
+   document.getElementById("me").src="./images/100897746.jpg";
+}
+
+document.getElementById("rr1c9").onclick = function () {
+   document.getElementById("me").src="./images/100921112.jpg";
+}
+
+document.getElementById("rr2c0").onclick = function () {
+   document.getElementById("me").src="./images/100566530.jpg";
+}
+
+document.getElementById("rr2c1").onclick = function () {
+   document.getElementById("me").src="./images/100900073.jpg";
+}
+
+document.getElementById("rr2c2").onclick = function () {
+   document.getElementById("me").src="./images/100868754.jpg";
+}
+
+document.getElementById("rr2c3").onclick = function () {
+   document.getElementById("me").src="./images/100856452.jpg";
+}
+
+document.getElementById("rr2c4").onclick = function () {
+   document.getElementById("me").src="./images/100903417.jpg";
+}
+
+document.getElementById("rr2c5").onclick = function () {
+   document.getElementById("me").src="./images/100893367.jpg";
+}
+
+document.getElementById("rr2c6").onclick = function () {
+   document.getElementById("me").src="./images/100890502.jpg";
+}
+
+document.getElementById("rr2c7").onclick = function () {
+   document.getElementById("me").src="./images/100890567.jpg";
+}
+
+document.getElementById("rr2c8").onclick = function () {
+   document.getElementById("me").src="./images/100815108.jpg";
+}
+
+document.getElementById("rr2c9").onclick = function () {
+   document.getElementById("me").src="./images/100778539.jpg";
+}
